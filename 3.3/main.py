5bb46c2ce620e2301e2cc00f009eb41f947d6358
Malte Grau <mpg@MaltePiilGrauPC.localdomain>
2025-10-10 12:18:17 +0200

    3.3

--- 3.3/main.py
+++ 3.3/main.py
@@ -1,9 +1,18 @@
 import numpy as np
 from iminuit import Minuit
+import scipy.stats as ss
+
 
 def lin(x,a,b):
 	return a*x+b
 
+
+def chi2PExp(exps,ys): # constraint sum(exps) == sum(ys)
+	res = 0
+	expsys = zip(exps,ys)
+	for e,y in expsys:
+		res += np.pow(y-e,2)/e
+	return res
 
 def chi2Per(func,*params,xs,ys): # Persons chi squared
 	res = 0
@@ -25,21 +34,35 @@ def runTest(func,*params,xs,ys):
 		
 	return listOfAB
 
+def runTestExp(exp,ys):
+	listOfAB=[] # list of above and below
+	expys = zip(exp,ys)
+	for e,y in expys:
+		if e >= y:
+			listOfAB.append('+')
+		else :
+			listOfAB.append('-')
+		
+	return listOfAB
 
 if __name__ == "__main__":
 	data = np.loadtxt('data.txt')
 	bins = data[:,0]
 	obs1 = data[:,1]
 	obs2 = data[:,2]
-	obs3 = data[:,3]
-	print(chi2Per(lin,1,10,xs = bins,ys = obs1))
-	print(runTest(lin,1,10,xs = bins,ys = obs1))
+	exp = data[:,3]
+
+	ndf = 20 # nubmer of degrees of freedom, 
+
+	obs1chi = chi2PExp(exp,obs1)
+	print(obs1chi)
+	print(ss.chi2.sf(obs1chi,ndf))
+	print(runTestExp(exp,obs1))
 	
-	print(chi2Per(lin,1,10,xs = bins,ys = obs2))
-	print(runTest(lin,1,10,xs = bins,ys = obs2))
-	
-	print(chi2Per(lin,1,10,xs = bins,ys = obs3))
-	print(runTest(lin,1,10,xs = bins,ys = obs3))
+	obs2chi = chi2PExp(exp,obs1)
+	print(obs2chi)
+	print(ss.chi2.sf(obs2chi,ndf))
+	print(runTestExp(exp,obs2))
 	
 
 
